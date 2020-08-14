f150f0c99659f5cdf7fa21fac24d420cfb016b94
fubar2 <ross.lazarus@gmail.com>
2020-08-14 12:04:20 +1000

    Lots of old and confusing docs

--- docker/post-start-actions.sh
+++ docker/post-start-actions.sh
@@ -1,7 +1,3 @@
-
-# python workflow_import.py <api_key> <galaxy_url> '/path/to/workflow/file [--add_to_menu]'
-python $GALAXY_ROOT/scripts/api/workflow_import.py \
-  $GALAXY_DEFAULT_ADMIN_KEY \
-  http://127.0.0.1:$PORT \
-  file://localhost/$GALAXY_ROOT/workflows/TF_example_wf.ga --add_to_menu
-
+# hook to install tf demo workflow
+workflow-install -w $GALAXY_HOME/workflows/ -g http://localhost:$PORT \
+        -u $GALAXY_DEFAULT_ADMIN_USER -p $GALAXY_DEFAULT_ADMIN_PASSWORD --add_to_menu
