87701813671318d2c7ff4ef642987d8071b56437
fubar2 <ross.lazarus@gmail.com>
2020-08-14 13:07:51 +1000

    wheeee.

--- docker/post-start-actions.sh
+++ docker/post-start-actions.sh
@@ -1,3 +1,3 @@
 # hook to install tf demo workflow
-workflow-install -w $GALAXY_HOME/workflows/ -g http://localhost:$PORT \
+workflow-install -w $GALAXY_HOME/workflows -g http://localhost:$PORT \
         -u $GALAXY_DEFAULT_ADMIN_USER -p $GALAXY_DEFAULT_ADMIN_PASSWORD --add_to_menu
