5bd840ad33b36bf8eb116ba2eeb8140d4cbb63f8
fubar2 <ross.lazarus@gmail.com>
2020-08-16 22:09:24 +1000

    adding workflow

--- docker/post-start-actions.sh
+++ docker/post-start-actions.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 # hook to install tf demo workflow
 echo "#### post start actions.sh hook happening"
-workflow-install -w $GALAXY_ROOT/workflows/TF_example_wf.ga -g http://localhost:80 -a fakekey --publish_workflows
+chown $GALAXY_USER $GALAXY_ROOT/workflows/TF_example_wf.ga
+workflow-install -w $GALAXY_ROOT/workflows/TF_example_wf.ga -g http://localhost -a fakekey --publish_workflows
