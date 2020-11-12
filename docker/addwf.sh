e60aec71db0456166b1a02a5d1e091ce4f841632
= <=>
2020-11-12 20:56:25 +1100

    cleanup

--- docker/addwf.sh
+++
@@ -1,2 +0,0 @@
-docker run -p 8080:80 -e GALAXY_AUTO_UPDATE_WORKFLOWS=/galaxy-central/workflows/TF_sample_wf.ga -v /home/ubuntu/galaxy_storage/:/export/  toolfactory
-
--- docker/runclean.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-sudo rm -rf /s100/galaxy_storage/*
-sudo cp tftools/tfwelcome.html /s100/galaxy_storage/welcome.html
-docker build -t toolfactory .
-./startgaldock.sh
-
